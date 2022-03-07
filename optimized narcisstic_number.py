bfab2612c623678546e7d71baf4852ec122963fd
ethumana <omertopcu@me.com>
2022-03-07 13:19:09 +0300

    optimized

--- optimized narcisstic_number.py
+++ optimized narcisstic_number.py
@@ -1,38 +1,32 @@
-t = input("Sayı :")
-sinir = int(t)
-a = range(0,sinir)
-_range = list(map(int,str(sinir)))
+import time
+start_time = time.time()
+a = range(0,100000000)
+_range = list(map(int,str(100000000)))
 _sinir = len(_range)
 us = range(1,_sinir)
 num = [0,1,2,3,4,5,6,7,8,9]
 dic =[] 
 hesap = []
 dic = []
+l = list()
 for j in us :
     for i in num :
         hesap[i:i] = [i**j]
-        print(hesap)
     dic.append(hesap)
     hesap = []
-    print(dic)
 print("Hesaplama başlıyor")
+item = dic[0][8]
 for x in a :
-    _sum = list()
-    i = -1
+    sum = 0
     sayi = list(map(int,str(x)))
     n_sayi = len(sayi)
-    for p in sayi:
-        i +=1
-        _sum[i:i] =[p**n_sayi]
-        if sum(_sum) == x :
-            l.append(x)
-        sayi1 = sayi
-        n_sayi1 = n_sayi
-        _sum1 = _sum
-
+    for y in sayi: 
+        item = dic[n_sayi-1][y] 
+        sum = sum + item
+    if sum == int(x) :
+        l.append(x)
 if l :
     print(l)
-    
 else :
     print("Sorry maybe next time")
 
